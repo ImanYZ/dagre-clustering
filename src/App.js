aae820cb744ef7c0c2e6e08f918d3bbda6befe16
Iman <oneweb@umich.edu>
2020-06-25 23:51:35 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import dagre from "dagre";
+import "./App.css";
 
 function App() {
+  // Create a new directed graph
+  var g = new dagre.graphlib.Graph({ compound: true });
+
+  // Set an object for the graph label
+  g.setGraph({});
+
+  // Default to assigning a new object as a label for each new edge.
+  g.setDefaultEdgeLabel(function () {
+    return {};
+  });
+
+  // Here we're setting the nodes
+  g.setNode("a", { label: "A" });
+  g.setNode("b", { label: "B" });
+  g.setNode("c", { label: "C" });
+  g.setNode("d", { label: "D" });
+  g.setNode("e", { label: "E" });
+  g.setNode("f", { label: "F" });
+  g.setNode("g", { label: "G" });
+  g.setNode("TagA", {
+    label: "Tag A",
+  });
+  g.setNode("TagB", {
+    label: "Tag B",
+  });
+
+  // Set the parents to define which nodes belong to which cluster
+  g.setParent("a", "TagA");
+  g.setParent("g", "TagA");
+  g.setParent("b", "TagB");
+  g.setParent("c", "TagB");
+  g.setParent("d", "TagB");
+  g.setParent("e", "TagB");
+  g.setParent("f", "TagB");
+
+  // Set up edges, no special attributes.
+  g.setEdge("a", "b");
+  g.setEdge("b", "c");
+  g.setEdge("b", "d");
+  g.setEdge("b", "e");
+  g.setEdge("b", "f");
+  g.setEdge("a", "g");
+  g.setEdge("b", "g");
+  dagre.layout(g);
+  console.log(g);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {g.nodes().map((n) => {
+        const nodeN = g.node(n);
+        return (
+          <div className="nodeEl" style={{ left: nodeN.x, top: nodeN.y }}>
+            {nodeN.label}
+          </div>
+        );
+      })}
     </div>
   );
 }
